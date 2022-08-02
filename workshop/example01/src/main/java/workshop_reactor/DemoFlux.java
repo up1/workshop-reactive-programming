691bc2298e84eaa8a13c365674b17bc527608b8d
up1 <somkiat.p@gmail.com>
2022-08-02 13:57:05 +0700

    Demo flux

--- workshop/example01/src/main/java/workshop_reactor/DemoFlux.java
+++ workshop/example01/src/main/java/workshop_reactor/DemoFlux.java
@@ -1,16 +1,20 @@
 package workshop_reactor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 public class DemoFlux {
     public static void main(String[] args) {
         Flux<String> flux = Flux.just("A", "B", "C");
         flux.log()
             .map(i -> i.toLowerCase())
+//            .subscribeOn(Schedulers.newParallel("sub"))
+//            .publishOn(Schedulers.newParallel("pub"))
             .subscribe(
                 item -> System.out.println(item),
                 err -> System.out.println(err.getMessage()),
                 () -> System.out.println("Complete")
         );
+        System.out.println("Done");
     }
 }
